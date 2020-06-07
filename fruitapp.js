4410614d7d81844da7e287b4a75ee32ff33a081d
kesh4207 <keseanarobinson1@outlook.com>
2020-06-07 19:09:38 +0100

    Adding Sound and a scoreBoard

--- fruitapp.js
+++ fruitapp.js
@@ -1,175 +1,122 @@
-let apple='https://cdn.pixabay.com/photo/2015/05/22/16/43/apple-779336_960_720.jpg';
-let pear='https://cdn.pixabay.com/photo/2016/04/01/11/00/pear-1300153_960_720.png';
-let avocado='https://cdn.pixabay.com/photo/2020/02/13/16/14/avocado-4846143_960_720.png';
-let pineapple='https://cdn.pixabay.com/photo/2020/01/14/10/55/cartoon-4764726_960_720.png';
-let watermelon='https://cdn.pixabay.com/photo/2020/02/16/20/02/watermelon-4854649_960_720.png';
-let raspberry='https://cdn.pixabay.com/photo/2013/07/12/19/34/raspberry-155019_960_720.png';
+let apple = 'https://cdn.pixabay.com/photo/2015/05/22/16/43/apple-779336_960_720.jpg';
+let pear = 'https://cdn.pixabay.com/photo/2016/04/01/11/00/pear-1300153_960_720.png';
+let avocado = 'https://cdn.pixabay.com/photo/2020/02/13/16/14/avocado-4846143_960_720.png';
+let pineapple = 'https://cdn.pixabay.com/photo/2020/01/14/10/55/cartoon-4764726_960_720.png';
+let watermelon = 'https://cdn.pixabay.com/photo/2020/02/16/20/02/watermelon-4854649_960_720.png';
+let raspberry = 'https://cdn.pixabay.com/photo/2013/07/12/19/34/raspberry-155019_960_720.png';
+let fruitArray = [apple, pear, avocado, pineapple, watermelon, raspberry];
 
-let fruitArray=[apple,pear,avocado,pineapple,watermelon,raspberry];
-
-
-
-function MakeSlots(spinCount,slotNum)
-{
-    
-    
-    this.startInd = Math.floor(Math.random() * fruitArray.length); // starts on a random number
-    this.lastInd = null; // what ends on after running
-    this.slotNum = slotNum;
-    this.spinCount = spinCount; // how many times run function. i.e. diffferent for each slot
-    
-    
-  
+function MakeSlots(spinCount, slotNum) {
+	this.startInd = Math.floor(Math.random() * fruitArray.length); // starts on a random number
+	this.lastInd = null; // what ends on after running
+	this.slotNum = slotNum;
+	this.spinCount = spinCount; // how many times run function. i.e. diffferent for each slot
 }
 
+function displayFruits() {
+	let status = document.getElementById('status');
+	status.innerText = ('Status: SPINNNINGG....');
+	let audio = new Audio('spinEffect.mp3');
+	let wining = new Audio('win.wav');
+	let losing = new Audio('looseHorn.wav');
+	$(audio).animate({
+		volume: 0.2
+	}, 1000);
+	audio.play();
+	let slot = new MakeSlots(20, 'slot1');
+	let spin = setInterval(spining, 100);;
+	slot.spinCount = slot.spinCount + Math.floor(Math.random() * fruitArray.length);
+	// this is for slot 2
+	let slot2 = new MakeSlots(25, 'slot2');
+	let spin2 = setInterval(spining2, 115);;
+	slot2.spinCount = slot2.spinCount + Math.floor(Math.random() * fruitArray.length);
+	// this is for slot3
+	let slot3 = new MakeSlots(30, 'slot3');
+	let spin3 = setInterval(spining3, 130);;
+	slot3.spinCount = slot3.spinCount + Math.floor(Math.random() * fruitArray.length);
+	let win = () => {
+		audio.pause();
+		if (slot.lastInd === slot2.lastInd && slot.lastInd === slot3.lastInd) {
+			status.innerText = 'Status: Win';
+			wining.play();
+			$(wining).animate({
+				volume: 0.2
+			}, 1000);
+		} else {
+			status.innerText = 'Status: Loss';
+			losing.play();
+			$(losing).animate({
+				volume: 0.2
+			}, 1000);
+		}
+	}
+	setTimeout(win, 6200);
 
+	function spining() {
+		if (slot.lastInd === null) startingFunction(slot); // mean it would be a first spin
+		// value that the slot is on 
+		let counter = slot.spinCount;
+		slot.lastInd++;
+		let arrInd = slot.lastInd % fruitArray.length; // cycling through the array to mimic a slot machine
+		if (slot.lastInd === counter) {
+			//show(slot.slotNum,arrInd);
+			slot.lastInd = arrInd;
+			show(slot.slotNum, arrInd);
+			clearInterval(spin);
+		}
+		showImg1(slot.slotNum, arrInd);
+	}
 
+	function spining2() {
+		if (slot2.lastInd === null) startingFunction(slot2); // mean it would be a first spin
+		// value that the slot is on 
+		let counter = slot2.spinCount;
+		slot2.lastInd++;
+		let arrInd = slot2.lastInd % fruitArray.length; // cycling through the array to mimic a slot machine
+		if (slot2.lastInd === counter) {
+			//show(slot.slotNum,arrInd);
+			slot2.lastInd = arrInd;
+			show(slot2.slotNum, arrInd);
+			clearInterval(spin2);
+		}
+		showImg1(slot2.slotNum, arrInd);
+	}
 
-
-function displayFruits(){
-   
-    //console.log(slot1.lastInd);
-    //let spin = setInterval(spining(),170);
-    
-    //let spin = setInterval(spining(slot1,20),170);
-    
-    let slot = new MakeSlots(20, 'slot1');
-    
-    
-   let spin = setInterval( spining, 100 );
-   //setInterval( function() { funca(10,3); }, 500 );
-    console.log('spincount is ' , slot.spinCount);
-    slot.spinCount = slot.spinCount + Math.floor(Math.random() * fruitArray.length);
-    
-
-    let spin3 = () => 
-    {
-      if (slot.lastInd === 1 |slot.lastInd === 2| slot.lastInd === 3|slot.lastInd === 4|slot.lastInd === 5)
-      {
-        console.log('it worksssssssssss');
-      }
-    }
-    setTimeout(spin3, 3000);
-    
-    function spining () {
-      
-
-      if (slot.lastInd === null ) startingFunction(slot); // mean it would be a first spin
-  
-      // value that the slot is on 
-      let counter = slot.spinCount; 
-      console.log('counter: ', counter);
-      console.log('last: ' , slot.lastInd);
-      
-      slot.lastInd ++;
-      
-   
- 
-      let arrInd =  slot.lastInd % fruitArray.length; // cycling through the array to mimic a slot machine
-       console.log('ind ', arrInd);
-       console.log('last: ' , slot.lastInd);
-    
- 
-        if ( slot.lastInd === counter) {
-           //show(slot.slotNum,arrInd);
-           
-           slot.lastInd = arrInd;
-           console.log(arrInd);
-           show(slot.slotNum,arrInd); 
-           
-          
-
-           
-           
-           clearInterval(spin);
-          
-       }
-       
-       
-      showImg1(slot.slotNum,arrInd); 
-     
-  }
-  
- 
-    return false;
+	function spining3() {
+		if (slot3.lastInd === null) startingFunction(slot3); // mean it would be a first spin
+		// value that the slot is on 
+		let counter = slot3.spinCount;
+		slot3.lastInd++;
+		let arrInd = slot3.lastInd % fruitArray.length; // cycling through the array to mimic a slot machine
+		if (slot3.lastInd === counter) {
+			//show(slot.slotNum,arrInd);
+			slot3.lastInd = arrInd;
+			show(slot3.slotNum, arrInd);
+			clearInterval(spin3);
+		}
+		showImg1(slot3.slotNum, arrInd);
+	}
+	return false;
 }
 
-
-  function showImg1(slotNum, arrInd)
-{
-   
-  console.log(arrInd);
-  let slotCell = document.getElementById(slotNum);
-  slotCell= slotCell.querySelector('img');
-  //slotCell.style.filter ='blur(2px)';
- 
-  slotCell.classList.remove('move');
-  slotCell.offsetHeight;
-  // browser needs to recognise animation has been reset by  a page repaint
-  $(slotCell).addClass('move');
-
-  
-  
-  
-  
-  
-  
-  // var elem = slotCell;   
-  // var pos = 0;
-  // var id = setInterval(frame, 3);
-  // function frame() {
-  //   if (pos == 100) {
-  //     elem.style.top = 0; 
-  //     clearInterval(id);
-  //   } else {
-  //     $(slotCell).attr('src',fruitArray[arrInd]);
-  //     pos++;
-  //     elem.style.top = pos + "px"; 
-      
-  //   }
-  // }
- 
-    $(slotCell).attr('src',fruitArray[arrInd]);
-
-
+function showImg1(slotNum, arrInd) {
+	let slotCell = document.getElementById(slotNum);
+	slotCell = slotCell.querySelector('img');
+	slotCell.classList.remove('move');
+	slotCell.offsetHeight;
+	// browser needs to recognise animation has been reset by  a page repaint
+	$(slotCell).addClass('move');
+	$(slotCell).attr('src', fruitArray[arrInd]);
 }
 
-function show(slotNum, arrInd)
-{
-   
-  console.log(arrInd);
-  let slotCell = document.getElementById(slotNum);
-  slotCell= slotCell.querySelector('img');
-  
-  
-  $(slotCell).attr('src',fruitArray[arrInd]);
- // slotCell.classList.remove('move');
-  //slotCell.offsetHeight;
-  
- 
-  slotCell.style.filter ='blur(0)';
-  
-  
-  
+function show(slotNum, arrInd) {
+	let slotCell = document.getElementById(slotNum);
+	slotCell = slotCell.querySelector('img');
+	$(slotCell).attr('src', fruitArray[arrInd]);
+	slotCell.style.filter = 'blur(0)';
 }
-
-/* problem with CSS animations keyframes by default only run ONCE hence the animation needs to be reset  */
-
-
-
 
 function startingFunction(slot) {
-    slot.lastInd = slot.startInd;
-    showImg1(slot.slotNum,slot.lastInd);
-    
-
-       
+	slot.lastInd = slot.startInd;
+	showImg1(slot.slotNum, slot.lastInd);
 }
-
-
-
-
- 
- 
- 
- 
